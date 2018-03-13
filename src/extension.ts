3326f7bfc5c9435634900a2f5ba76c8b678b8c6e
Naoya Komatsu <komatsu@biscrat.com>
2018-03-14 06:58:08 +0900

    Add reference prompt

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,16 @@
 'use strict';
 import { ExtensionContext } from 'vscode';
 import * as vscode from 'vscode';
+import { ReferenceProvider } from './reference';
 import { CTagsSupportProvider } from './ctags';
 
 export function activate(ctx: ExtensionContext): void {
+    const referenceProvider = new ReferenceProvider();
+    ctx.subscriptions.push(vscode.commands.registerCommand("kagex.showReferencePrompt", () => referenceProvider.showPrompt()));
+    ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => referenceProvider.onDidChangeConfiguration()));
+
     const ctagsSupportProvider = new CTagsSupportProvider();
-    ctx.subscriptions.push(vscode.commands.registerCommand("kagex.updateCtags", () => {
-        ctagsSupportProvider.updateCtags();
-    }));
-    ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument(document => {
-        ctagsSupportProvider.onDidSaveTextDocument(document);
-    }));
-    ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => {
-        ctagsSupportProvider.onDidChangeConfiguration();
-    }));
+    ctx.subscriptions.push(vscode.commands.registerCommand("kagex.updateCtags", () => ctagsSupportProvider.updateCtags()));
+    ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument(document => ctagsSupportProvider.onDidSaveTextDocument(document)));
+    ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => ctagsSupportProvider.onDidChangeConfiguration()));
 }
